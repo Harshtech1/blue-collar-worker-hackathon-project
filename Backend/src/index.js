78188a2ea11d62fb4cfd5a5bfeaab95b6f67435f
Your Name <your.email@example.com>
2026-04-05 17:53:03 +0530

    fix: resolve __dirname before initialization error

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -18,10 +18,10 @@ import jwt from "jsonwebtoken";
 import path from "path";
 
 import { fileURLToPath } from "url";
+import path from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 
 dotenv.config({ path: path.join(__dirname, "../.env") });
 
